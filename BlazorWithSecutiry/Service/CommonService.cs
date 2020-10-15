d55c82dc649f7ce1391c22cb0ed22c712e3ad955
marnusbotha20 <58814383+marnusbotha20@users.noreply.github.com>
2020-10-15 15:56:57 +0200

    commit revert

--- BlazorWithSecutiry/Service/CommonService.cs
+++ BlazorWithSecutiry/Service/CommonService.cs
@@ -1,15 +1,11 @@
-﻿using BlazorWithSecutiry.Data;
-using BlazorWithSecutiry.DataAccess;
+﻿using BlazorWithSecutiry.DataAccess;
 using BlazorWithSecutiry.Models;
-using System.Collections.Generic;
-using System.Linq;
-//using Microsoft.AspNetCore.Blazor.Components;
+using Microsoft.AspNetCore.Blazor.Components;
 using System.Net.Mail;
-using System.Threading.Tasks;
 
 namespace BlazorWithSecutiry.Service
 {
-    public class CommonService //: BlazorComponent
+    public class CommonService : BlazorComponent
     {
         CommonDAL common = new CommonDAL();
         public void Create(ContactUsDetails model)
